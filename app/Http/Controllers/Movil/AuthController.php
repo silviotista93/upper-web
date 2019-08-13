be57de0038087f2efa92c9c067f2486d1258c3b1
didier01 <deramirez19@misena.edu.co>
2019-08-13 17:38:49 -0500

    google2

--- app/Http/Controllers/Movil/AuthController.php
+++ app/Http/Controllers/Movil/AuthController.php
@@ -101,6 +101,19 @@ class AuthController extends Controller
         $check = User::whereEmail($email)->first();
         if ($check) {
             $user = $check;
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+            if ($request->remember_me) {
+                $token->expires_at = Carbon::now()->addWeeks(1);
+            }
+            $token->save();
+            return response()->json([
+                'access_token' => $tokenResult->accessToken,
+                'token_type'   => 'Bearer',
+                'expires_at'   => Carbon::parse(
+                    $tokenResult->token->expires_at)
+                    ->toDateTimeString(),
+            ]);
         } else {
             $user = new User([
                 'names' => $request->names,
@@ -139,20 +152,7 @@ class AuthController extends Controller
         }
         if ($success === true) {
 
-        $user = $check;
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-        if ($request->remember_me) {
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        }
-        $token->save();
-        return response()->json([
-            'access_token' => $tokenResult->accessToken,
-            'token_type'   => 'Bearer',
-            'expires_at'   => Carbon::parse(
-                $tokenResult->token->expires_at)
-                ->toDateTimeString(),
-        ]);
+        
         }
     }
 
